d84e9fe180a5fef5078ee4ca9a718fb1eff56f2a
Dave Eargle <dave.eargle@gmail.com>
2012-01-26 21:25:02 -0700

    Now passing in the user selected team to the predictor class (setting it in the Predictor constructor)

--- CollegeFootballSeasonPredictor/Predictor.cs
+++ CollegeFootballSeasonPredictor/Predictor.cs
@@ -4,6 +4,12 @@ namespace CollegeFootballSeasonPredictor
 {
     public class Predictor
     {
+        private Team _userSelectedTeam { get; set; }
+
+        public Predictor(Team userSelectedTeam)
+        {
+            this._userSelectedTeam = userSelectedTeam;
+        }
         public void predict(Game game)
         {
             game.Winner = game.HomeTeam;
